086b680bff9c47b5bb922f66f1875c08e8db08e7
Gustavo Fernandes Costa <84602100+gustavofcosta@users.noreply.github.com>
2022-07-29 03:06:58 -0300

    Update sidebarStyle.js

--- src/components/styles/sidebarStyle.js
+++ src/components/styles/sidebarStyle.js
@@ -1,8 +1,8 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.nav`
-  display: ${({ sidebar }) => {
-    return sidebar ? "none" : "false";
+  display: ${({ sidebarIsOpen }) => {
+    return sidebarIsOpen ? "block" : "none";
   }};
   position: absolute;
   right: 0;
